0d597d226e489af6b7aa2ed9476ea9aeddadaa22
Svennson17 <kiryl.shvedau@gmail.com>
2022-05-26 17:23:24 +0300

    refactor

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext as _
+
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.get_full_name()
 
 
 class Status(models.Model):
