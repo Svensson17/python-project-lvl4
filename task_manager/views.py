90956de4926c1bc3e3ab5171ef0e5a0e47b68d5f
Svennson17 <kiryl.shvedau@gmail.com>
2022-06-01 13:35:04 +0300

    refactor

--- task_manager/views.py
+++ task_manager/views.py
@@ -50,7 +50,12 @@ class LogoutUser(LogoutView):
         return super().dispatch(request, *args, **kwargs)
 
 
-class EditUser(LoginRequiredMixin, SuccessMessageMixin, CheckUserForDeleteMixin, UpdateView):
+class EditUser(
+    LoginRequiredMixin,
+    SuccessMessageMixin,
+    CheckUserForDeleteMixin,
+    UpdateView
+):
     model = get_user_model()
     template_name = 'users/edit_user.html'
     form_class = UserForm
