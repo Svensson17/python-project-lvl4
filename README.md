bf605a8ae045f859357c755620026f130c61027e
Svennson17 <kiryl.shvedau@gmail.com>
2022-06-06 12:08:53 +0300

    refactor

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Svensson17/python-project-lvl4/workflows/hexlet-check/badge.svg)](https://github.com/Svensson17/python-project-lvl4/actions)
 [![CI](https://github.com/Svensson17/python-project-lvl4/actions/workflows/CI.yml/badge.svg)](https://github.com/Svensson17/python-project-lvl4/actions/workflows/CI.yml)
+[![Maintainability](https://api.codeclimate.com/v1/badges/59118e1b41455367e86d/maintainability)](https://codeclimate.com/github/Svensson17/python-project-lvl4/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/59118e1b41455367e86d/test_coverage)](https://codeclimate.com/github/Svensson17/python-project-lvl4/test_coverage)
 
-This is my project on hexlet.io.I have implemented task manager which helps users to classify, track, update tasks and performers in different projects.
+Приложение развернуто на Heroku: https://dashboard.heroku.com/apps/warm-temple-58793
+
+## Функциональные возможности
+приложение настроено на работу с базой данных PostgreSQL;
+реализована авторизация пользователей;
+в системе может быть зарегистрировано множество пользователей;
+пользователь после авторизации может создавать себе задачу, указав для этого ее название, описание, статус, назначить исполнителя из списка зарегистрированных пользователей и при необходимости выбрать один или несколко тегов из списка;
+пользователь может редактировать содержимое любой своей или чужой задачи;
+пользователь может удалить любую из ранее созданных задач;
+пользователь может вывести список задач с возможностью фильтрации по статусу, автору, исполнителю, а также по тегам;
+пользователь может может добавлять, редактировать и изменять статусы, а также добавлять теги.
+
+## Запуск приложения
+Активируйте виртуальное окружение и установите необходимые зависимости, выполнив команду:
+
+```console
+poetry install
+```
+
+Замените название файла .env.example на .env и задайте свои значения переменных внутри этого файла.
+
+После этого сделайте и примените миграции командой:
+
+```console
+make migrations
+```
--- app/tests.py
+++ app/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
 
-from app.models import User, Status
+from app.models import User, Status, Task, Label
 
 
 class TestLoginAndRegistration(TestCase):
@@ -71,3 +71,87 @@ class TestStatusCRUD(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(Status.objects.all()), 0)
+
+
+class TestTaskCRUD(TestCase):
+    def test_create_task(self):
+        user = User.objects.create_user(username='admin', password='admin')
+        client = Client()
+        client.force_login(user)
+        response = client.post(
+            path=reverse('create_task'), data={'name': "admin"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        task = Task.objects.get(name='admin')
+        self.assertTrue(task)
+        self.assertEqual(task.name, 'admin')
+
+    def test_update_task(self):
+        user = User.objects.create_user(username='admin', password='admin')
+        client = Client()
+        client.force_login(user)
+        task = Task.objects.create(name='old_status')
+        response = client.post(
+            path=reverse(
+                'update_task',
+                args=[str(task.pk)]),
+            data={'name': "admin"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        task = Task.objects.get(name='admin')
+        self.assertTrue(task)
+        self.assertEqual(task.name, 'admin')
+
+    def test_delete_task(self):
+        user = User.objects.create_user(username='admin', password='admin')
+        client = Client()
+        client.force_login(user)
+        task = Task.objects.create(name='old_task')
+        response = client.post(
+            path=reverse(
+                'delete_task',
+                args=[str(task.pk)]), follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(Task.objects.all()), 0)
+
+
+class TestLabelCRUD(TestCase):
+    def test_create_label(self):
+        user = User.objects.create_user(username='admin', password='admin')
+        client = Client()
+        client.force_login(user)
+        response = client.post(
+            path=reverse('create_label'), data={'name': "admin"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        label = Label.objects.get(name='admin')
+        self.assertTrue(label)
+        self.assertEqual(label.name, 'admin')
+
+    def test_update_label(self):
+        user = User.objects.create_user(username='admin', password='admin')
+        client = Client()
+        client.force_login(user)
+        label = Label.objects.create(name='old_status')
+        response = client.post(
+            path=reverse(
+                'update_label',
+                args=[str(label.pk)]),
+            data={'name': "admin"}, follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        label = Label.objects.get(name='admin')
+        self.assertTrue(label)
+        self.assertEqual(label.name, 'admin')
+
+    def test_delete_label(self):
+        user = User.objects.create_user(username='admin', password='admin')
+        client = Client()
+        client.force_login(user)
+        label = Label.objects.create(name='old_label')
+        response = client.post(
+            path=reverse(
+                'delete_label',
+                args=[str(label.pk)]), follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(Label.objects.all()), 0)
